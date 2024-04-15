f8ebc95bfd1c1606341c56264c309d35b23f2b93
samirahmad5445 <samirahmad5445@gmail.com>
2024-04-15 07:03:31 +0200

    update: app icon & encrypt fragment & images

---
+++ app/src/main/java/com/learncrypto/app/CiphersListAdapter.java
@@ -0,0 +1,33 @@
+package com.learncrypto.app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+public class CiphersListAdapter extends  ArrayAdapter<String> {
+
+    public CiphersListAdapter(Context context, String[] ciphers) {
+        super(context, 0, ciphers);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        String cipherName = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.cipher_list_item, parent, false);
+        }
+
+        TextView cipher_list_item_name = convertView.findViewById(R.id.cipher_list_item_name);
+        cipher_list_item_name.setText(cipherName);
+
+        return convertView;
+    }
+}
